a0248a914d481f2f9b7b9a7b2b81190e0ffc50d3
EzeMati14 <ezematitrabajo@gmail.com>
2024-07-08 19:39:35 +0200

    Proyecto Angular

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,25 @@
+import { Character } from './../interfaces/character.interface';
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dbz-main-page',
+    templateUrl: 'main-page.component.html'
+})
+
+export class MainPageComponent  { 
+    public characters : Character[] = [{
+        name: 'Krillin',
+        power: 1000
+    }, {
+        name: 'Goku',
+        power: 9500
+    },{
+        name: 'Vegeta',
+        power: 7500
+    }];
+
+    onNewCharacter(character: Character):void {
+        this.characters.push(character);
+        }
+ }
